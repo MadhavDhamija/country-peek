e3531aabcde79318a4c98b4574a0cd03930f0ed1
MadhavDhamija <dhamijamadhav3@gmail.com>
2026-04-13 15:13:28 +0530

    Part 2: API integration with debounce and country cards

--- country-peek/src/App.jsx
+++ country-peek/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import Favourites from "./pages/Favourites";
+import Favourites from "./pages/Favourite";
 import NotFound from "./pages/NotFound";
-import "./styles/App.css";
+import "./App.css";
 
 function App() {
   return (
